2dee651e0827978823b2b556494f73bf3155ad63
Flow <24876391+fchambon@users.noreply.github.com>
2021-02-18 19:05:09 +0100

    Update terraform provided with Azure Backend

--- terraform/azrprovider.tf
+++ terraform/azrprovider.tf
@@ -11,3 +11,12 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "RG-TF-CONF-DEV01"
+    storage_account_name = "aztfsto2021"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
